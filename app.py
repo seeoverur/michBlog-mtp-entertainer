726cfbca72cb6debefbd51449cb96fb1407db91d
seeoverur <seeoverur@gmail.com>
2025-10-02 18:06:07 -0400

    update

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+    from flask import Flask, render_template
 
-app = Flask(__name__)
+    app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return render_template(index.html)
+    @app.route('/')
+    def home():
+        return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    if __name__ == '__main__':
+        app.run(debug=True)
